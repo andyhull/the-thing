b0e129a12dfc59ed046e3b732ded9a2efa224843
Andy Hull <andyhull3@gmail.com>
2014-07-16 18:14:27 -0400

    the first thing.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheThing::Application.config.secret_key_base = '2fcccc9c3221ed2e9ab7b24a54f0cefba3cfb3b08f88e8f93c9342bde38e0e90c1348ef82271b9117212546f2ebb23139eb88828a779a439f69e6afe7c03c63a'
