ab2a9efbebc4c7a038f24386060344bfe84349e1
Andy Hull <andyhull3@gmail.com>
2014-07-16 18:50:50 -0400

    set up devise, cancan, role_model and basic database.

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  include RoleModel
+
+  # if you want to use a different integer attribute to store the
+  # roles in, set it with roles_attribute :my_roles_attribute,
+  # :roles_mask is the default name
+  roles_attribute :roles_mask
+
+  # declare the valid roles -- do not change the order if you add more
+  # roles later, always append them at the end!
+  roles :admin, :user, :guest
+end
