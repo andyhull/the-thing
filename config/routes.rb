339502087d0940f25ec355a15b66ebebbbfa61cf
Andy Hull <andyhull3@gmail.com>
2014-07-20 13:54:02 -0400

    added user editing for admins.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ TheThing::Application.routes.draw do
   resources :things
   devise_for :users
 
+  resources :users
+
   root to: "home#index"
 
   
--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -10,4 +10,33 @@ feature "Users" do
     visit "/"
     expect(page).to have_content
   end
+
+  scenario "viewing the index when not logged in" do
+    visit "/users"
+    expect(page.current_path).to eq "/users/sign_in"
+  end
+
+  scenario "viewing the index page" do
+    sign_in @user
+    visit "/users"
+    expect(page).to have_content
+  end
+
+   scenario "goto sign in user when not logged in" do
+    visit "/users/new"
+    expect(page.current_path).to eq "/users/sign_in"
+  end
+
+  scenario "creating a new user" do
+    sign_in @user
+    visit "/users/new"
+    expect(page).to have_content
+  end
+
+  scenario "viewing the show page" do
+    sign_in @user
+    visit "/users/#{@user.id}"
+    expect(page).to have_content
+  end
+    
 end
